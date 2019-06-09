25228c5a30ba86022978ba4a2207b683f34e6961
masayuki.hirata <masayuki.h0729@gmail.com>
2019-06-09 16:55:49 +0900

    swift first

---
+++ SwiftStart/SwiftStart/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftStart
+//
+//  Created by 平田将之 on 2019/06/09.
+//  Copyright © 2019 mh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
